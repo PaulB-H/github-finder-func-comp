549d5e42327d1557e14fa492ddac51fefef22394
Paul Bernard-Hall <p.bernardhall@gmail.com>
2020-11-12 04:27:33 -0500

    Moved state from Users to App

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -13,7 +13,7 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 			<h3>{login}</h3>
 			<div>
 				<a href={html_url} className="btn btn-dark btn-sm my-1">
-					Moree
+					More
 				</a>
 			</div>
 		</div>
